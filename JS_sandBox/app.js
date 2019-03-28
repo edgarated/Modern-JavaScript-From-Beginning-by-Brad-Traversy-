a7a52cc6e32e25e8b9032d63449fe8a45c98133b
edgarated <[chukwuebukaaneke22@gmail.com]>
2019-03-28 19:12:48 +0100

    TEMPLATE LITERALS

--- JS_sandBox/app.js
+++ JS_sandBox/app.js
@@ -1,65 +1,12 @@
-//STRING METHODS AND CONCATENATION
+//TEMPLATE LITERALS
 
-const firstName = "Femi";
-const lastName = "Atunwa";
-const age = 27;
-const str = "Hello my name is Ebuka"
-const tag = "car, food, juice, junky"
+const name = "John";
+const age = 28;
+const job = "Web developer";
+const city = "Lagos"
+
 let val;
 
-val = firstName + lastName;
-val = firstName + ' ' + lastName;
+val = `DO you know that ${name} is ${age}, He is a ${job} that hustles in ${city}`
 
-// To Append
-val = "Ebuka "
-val += "Tunde"
-
-//To concatenate
-val = "Hello, My name is " + firstName + ' ' + lastName + " , and i am " + age + " years old";
-
-//Escaping
-val =  "that's awesome, i can't wait for the show to start"
-
-//Length
-val = firstName.length
-
-//concat()
-
-val = firstName.concat(' ' , lastName);  //Femi Atunwa
-
-//change case
-val = firstName.toLowerCase() //femi
-val =lastName.toUpperCase() //ATUNWA
-
-//indexOf()
-
-val =firstName.indexOf("e"); //1
-val = lastName.lastIndexOf("n") //indexOf return a number when an alphabet is the input
-val = lastName.charAt('3') //n    //charAt return an alphabet when number is the input
-
-//to get the last char
-
-val = firstName.charAt(firstName.length - 1) //i
-
-//substring()
-
-val = firstName.substring(1,3) //em
-
-//slice
-val = lastName.slice(0,2) //At
-val = lastName.slice(-2) //wa  it starts slicing fron the back
-
-val = str.split(' ')
-val = tag.split(",")
-
-//replace()
-val = str.replace("Ebuka", "adaobi")  //adaobi replaces Ebuka
-
-//includes()
-
-val = str.includes("Ebuka") //true
-
-console.log(val);
-
-
-
+console.log(val)
