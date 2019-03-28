cba66b3270230dc7c4968259b361886b4815aede
edgarated <[chukwuebukaaneke22@gmail.com]>
2019-03-28 15:05:22 +0100

    VARIABLES - VAR, LET, CONST

---
+++ JS_sandBox/app.js
@@ -0,0 +1,38 @@
+//VARIABLES - VAR, LET, CONST
+
+//DECLARE A VARIABLE [VAR]
+var name = "John Doe";
+console.log(name)
+//prints John Doe
+
+//REASSIGN VARIABLE
+name = " Jane Doe";
+console.log(name)
+//prints  Jane Doe
+
+//INIT VAR
+var greeting;
+greeting = "hello";
+console.log(greeting);
+//prints hello
+
+//DECLARE A VARIABLE [LET]
+let name1 = "Ebuka Obi";
+console.log(name1)
+//prints Ebuka Obi
+
+
+//DECLARE A VARIABLE 
+const name2 = "John Doe";
+console.log(name2)
+//prints John Doe
+
+//const cannot be reassigned
+name2 = "chukwu";
+console.log(name2)
+//prints ERROR
+
+//const must have a value;
+const hobby;
+console.log(hobby)
+//prints ERROR
