3c4810171aab3e84f20253ada01a98d57bbb0a59
edgarated <[chukwuebukaaneke22@gmail.com]>
2019-03-28 16:39:11 +0100

    NUMBERS AND MATH OBJECTS

--- JS_sandBox/app.js
+++ JS_sandBox/app.js
@@ -1,53 +1,25 @@
-//TYPE CONVERSION
-
-//declare a variable
+//NUMBERS AND MATH OBJECTS
+let num1 = 23;
+let num2 = 25;
 let val;
 
-//TO CONVERT TO STRING 1
-val = String(599);
-val = String(4+4);
+//simple mathematics
+val = num1 + num2
+val = num1 - num2
+val = num1 * num2
+val = num1 / num2
+val = num1 % num2
 
-//Boolean to string
-
-val = String(true);
-
-//Date to a string
-val = String(new Date())
-
-//Array to string
-val = String([1,2,3,4,5])
-
-//TO CONVERT TO STRING 2
-
-val = (5).toString()
-val = (true).toString();
-
-
-//STRING TO NUMBER
-val = Number("5");
-console.log(val.toFixed())
-
-//BOOLEAN TO NUMBER
-val = Number(true) 
-val = Number(false) 
-val = Number(null) 
-val = Number("hello") //NaN
-val = parseInt("100")  //100
-val = parseFloat("100.90"); //100.9
+//MATH OBJECT
+val = Math.PI
+val = Math.E
+val = Math.round(23.5) //24
+val = Math.ceil(23.5) //24
+val = Math.floor(23.5) //23
+val = Math.random();
 
 
 
+console.log(val)
 
-//output
-console.log(val); //599
-console.log(typeof val);//string
-console.log(val.length) //3
-
-//TYPE COERHESION
-
-let val1 = String(9);
-let val2 = 1;
-let sum = (val1 + val2);
-console.log(typeof sum);
-console.log(sum);
 
