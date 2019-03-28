b276fb36da72db7ab66c47d20b01929583b6ca42
edgarated <[chukwuebukaaneke22@gmail.com]>
2019-03-28 23:53:52 +0100

    CONDITIONALS

--- JS_sandBox/app.js
+++ JS_sandBox/app.js
@@ -1,22 +1,82 @@
-//DATE AND TIME
-let val;
-const today = new Date();
-let birthday = new Date(" 9-10-1981 11:24:00")
-birthday = new Date(" september 10 1982")
+//CONDITIONALS
 
-val = today.getMonth()
-val = today.getDate()
-val = today.getDay()
-val = today.getFullYear()
-val = today.getHours()
-val = today.getMinutes()
-val = today.getSeconds()
+const id = 100;
+
+//EQUAL TO
+if (id == 100){
+  console.log("CORRECT")
+}else{
+  console.log("INCORRECT")
+}
+
+//NOT EQUAL TO
+if (id != 101){
+  console.log("CORRECT")
+}else{
+  console.log("INCORRECT")
+}
+
+//EQUAL TO VALUE AND TYPE
+if (id === 100){
+  console.log("CORRECT")
+}else{
+  console.log("INCORRECT")
+}
+
+// NOT EQUAL TO VALUE AND TYPE
+if (id !== 100){
+  console.log("CORRECT")
+}else{
+  console.log("INCORRECT")
+}
+
+//Test if undefined
+
+if(typeof id !== "undefined"){
+console.log(`THE ID IS ${id}`)
+} else{
+  console.log("NO ID");
+}
+
+//GREATER OR LESS THAN
+if (id > 200){
+  console.log("CORRECT")
+}else{
+  console.log("INCORRECT")
+}
+
+//IF ELSE
+
+const color = "yellow";
+if(color === "yellow"){
+  console.log("color is yellow")
+} else if(color === blue){
+  console.log("color is blue")
+}else{
+  console.log("no color found")
+}
+
+//LOGICAL OPERATORS
+
+ const name = "steve";
+ const age = 20
+
+ // AND &&
+ if(age > 0 && age < 10){
+   console.log(`${name} is a baby`)
+ }else if(age > 10 && age < 20){
+   console.log(`${name} is a teenager`)
+ }else{
+   console.log(`${name} is an adult`)
+ }
+
+ // OR ||
+ if(age < 10 || age > 50){
+   console.log(`${name} cannot run in the race`)
+ }else {
+   console.log(`${name} is registered for the race`)
+ }
 
 
-birthday.setDate(12)
-birthday.setMonth(2)
-birthday.setFullYear(1984)
-birthday.setMinutes(30);
-
-
-console.log(val);
+ //TERNARY OPERATORS
+ console.log(id === 110? "CORRECT" : "INCORRECT")
