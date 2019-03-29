bdcb00b0d3e786074cae6b4f3439fdd5f530e72d
edgarated <[chukwuebukaaneke22@gmail.com]>
2019-03-29 05:01:52 +0100

    SWITCH STATEMENT

--- JS_sandBox/app.js
+++ JS_sandBox/app.js
@@ -1,82 +1,65 @@
-//CONDITIONALS
+//SWITCH STATEMENT
 
-const id = 100;
+const color = "pink";
+switch(color){
 
-//EQUAL TO
-if (id == 100){
-  console.log("CORRECT")
-}else{
-  console.log("INCORRECT")
+case "purple":
+console.log("color is purple");
+break
+
+case "blue":
+console.log("color is blue");
+break
+
+case "pink":
+console.log("color is pink");
+break
+
+case "yellow":
+console.log("color is yellow");
+break
+
+case "black":
+console.log("color is black");
+break
+default:
+console.log("not a color")
 }
 
-//NOT EQUAL TO
-if (id != 101){
-  console.log("CORRECT")
-}else{
-  console.log("INCORRECT")
+
+//To GET THE DAY OF THE WEEK USING SWITCH METHOD
+
+let day;
+switch(new Date().getDay()){
+
+  case 0:
+  day = "sunday";
+  break;
+
+  case 1:
+  day = "monday";
+  break;
+
+  case 2:
+  day = "tuesday";
+  break;
+
+  case 3:
+  day = "wednesday";
+  break;
+
+  case 4:
+  day = "thursday";
+  break;
+
+  case 5:
+  day = "friday";
+  break;
+
+  case 6:
+  day = "saturday";
+  break;
+
 }
 
-//EQUAL TO VALUE AND TYPE
-if (id === 100){
-  console.log("CORRECT")
-}else{
-  console.log("INCORRECT")
-}
-
-// NOT EQUAL TO VALUE AND TYPE
-if (id !== 100){
-  console.log("CORRECT")
-}else{
-  console.log("INCORRECT")
-}
-
-//Test if undefined
-
-if(typeof id !== "undefined"){
-console.log(`THE ID IS ${id}`)
-} else{
-  console.log("NO ID");
-}
-
-//GREATER OR LESS THAN
-if (id > 200){
-  console.log("CORRECT")
-}else{
-  console.log("INCORRECT")
-}
-
-//IF ELSE
-
-const color = "yellow";
-if(color === "yellow"){
-  console.log("color is yellow")
-} else if(color === blue){
-  console.log("color is blue")
-}else{
-  console.log("no color found")
-}
-
-//LOGICAL OPERATORS
-
- const name = "steve";
- const age = 20
-
- // AND &&
- if(age > 0 && age < 10){
-   console.log(`${name} is a baby`)
- }else if(age > 10 && age < 20){
-   console.log(`${name} is a teenager`)
- }else{
-   console.log(`${name} is an adult`)
- }
-
- // OR ||
- if(age < 10 || age > 50){
-   console.log(`${name} cannot run in the race`)
- }else {
-   console.log(`${name} is registered for the race`)
- }
-
-
- //TERNARY OPERATORS
- console.log(id === 110? "CORRECT" : "INCORRECT")
+console.log(`Today is ${day}`)
