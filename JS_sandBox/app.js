bd2ec90689ff559b5b2bd500cb2ed9e0713c81fd
edgarated <[chukwuebukaaneke22@gmail.com]>
2019-03-29 06:57:14 +0100

    LOOPS

--- JS_sandBox/app.js
+++ JS_sandBox/app.js
@@ -1,46 +1,93 @@
-//FUNCTION DECLARATION
+//FOR LOOP
 
-function greeting (firstName = "John", lastName = "Doe"){
-return "Hello " + firstName + ' ' + lastName;
+// for (let index = 0; index < 10; index++) {
+//  if(index === 2){
+//    console.log("super number");
+//    continue;
+//  }
+
+//  if(index === 6){
+//    console.log("stop the loop")
+//    break;
+//  }
+//  console.log("Number " + index)
+// }
+
+//WHILE LOOP
+
+// let i = 0
+// while (i < 10) {
+  
+//   console.log( `Number : ${i}`)
+//   i++;
+// }
+
+//DO WHILE LOOP
+// let i = 0
+// do {
+//   console.log(i);
+//   i++
+// } while (i < 10);
+
+//LOOPING THROUGH ARRAYS
+
+const cars = ["toyota", "nissan", "chevy", "cherokee", "volks"]
+
+//FOR LOOP
+// for (let index = 0; index < cars.length; index++) {
+//   const element = cars[index];
+//   console.log(element)
+  
+// }
+
+// FOR EACH
+// cars.forEach(car => {
+//   console.log(car)
+// });
+
+//REVERSING AN ARRAY
+for (let index = cars.length; index >= 0 ; index--) {
+  const element = cars[index];
+  console.log(element)
+  
+}
+
+//MAP
+const users = [
+{id : 1, name : "tunji"},
+{id : 2, name : "rashidi"},
+{id : 3, name : "zizu"},
+
+];
+const ids = users.map(function(user){
+
+  return user.id
+});
+// console.log(ids);
+
+//FOR IN LOOP
+
+const user = {
+  name : "atiku",
+  location : "lagos",
+  age : 23,
+  hobby : "singing"
 
 }
-console.log(greeting("Ebuka", "Aneke"))
 
-//FUNCTION EXPRESSIONS
-
-const name = function(firstName, lasttName){
-
-  return `his full name is ${firstName} ${lasttName}`
-}
-console.log(name("John", "Okoro"))
-
-//IMMEDIATELY INVOKABLE FUNCTION EXPRESSIONS [IIFEs]
-// (function(){
-//   console.log("IIFE ran...");
-// })();
-
-// (function(name){
-//   console.log("hello " + name);
-// })("ebuka");
-
-
-//PROPERTY METHODS
-
-const todo = {
-  add(){
-    console.log(`Add to do..`)
-  },
-  edit(input){
-    console.log(`Edit ${input}`)
-  },
-
-  delete(){
-    console.log(`Delete...`)
-  }
+for (const x in user) {
+  console.log(`${x} : ${user[x]} `)
 }
 
-todo.add()
-todo.edit(9)
-todo.delete()
+//Example 2
+const admin = {
+  name : "atiku",
+  location : "lagos",
+  age : 23,
+  hobby : "singing",
+  quote : "live and let live"
+}
 
-
+for (const x in admin) {
+  console.log(`${x} : ${admin[x]}`)
+}
