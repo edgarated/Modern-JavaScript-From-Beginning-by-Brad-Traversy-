07bcf544050069d6db7912518722ac958afd9d70
edgarated <[chukwuebukaaneke22@gmail.com]>
2019-03-30 06:01:03 +0100

    Traversing-The-DOM

--- JS_sandBox/app.js
+++ JS_sandBox/app.js
@@ -1,88 +1,37 @@
-//DOM SELECTORS
+let val;
+const list = document.querySelector("ul.collection");
+const listItem = document.querySelector("li.collection-item:first-child")
 
+val = listItem;
+val = list;
 
-//getElementById
-// console.log(document.getElementById("task-title"))
+//Get child nodes
 
-// //Get things from the element
-// console.log(document.getElementById("task-title").id)
-// console.log(document.getElementById("task-title").className)
+val = list.childNodes
 
-// const taskTitle = document.getElementById("task-title");
+//get children element nodes
 
-// //change styling
-// taskTitle.style.background = "#333";
-// taskTitle.style.color = "white";
-// taskTitle.style.padding = "5px";
+val = list.children
+val = list.children[4].textContent = "Hello"
 
-// //change the content
-// taskTitle.textContent = "Task List"
-// taskTitle.innerText = "My Tasks"
-// taskTitle.innerHTML = '<span style= "color:red">Task List</span>'
+//get children of children
 
+val = list.children[2].children[0].id = "text-link"
+val = list.children[2].children[0]
 
-//document.querySelector
-// console.log(document.querySelector("#task-title"))
-// console.log(document.querySelector(".card-title"))
-// console.log(document.querySelector("h5"))
+val = list.firstElementChild
+val = list.lastElementChild
 
-// document.querySelector("ul li").style.color = "pink"
-// document.querySelector("li:nth-child(4)").style.color = "pink"
-// document.querySelector("li:nth-child(odd)").style.background = " "
-// document.querySelector("li:nth-child(odd)").innerText = "hi"
+//Get parent node
 
+val = listItem.parentNode
+val = listItem.parentElement
+val = listItem.parentElement.parentElement
 
-//document.getElementByClassName
-// const list = document.getElementsByClassName("collection-item")
-// console.log(list)
-// console.log(list[0])
-// list[2].style.color = "red";
-// list[3].textContent = "hello"
+//get next sibling
+val = listItem.nextElementSibling.nextElementSibling.nextElementSibling.previousElementSibling
 
+//get prev sibling
+// val = listItem.previousElementSibling
 
-//document.getElementByTagName
-
-// let lis = document.getElementsByTagName("li")
-// console.log(lis)
-
-// lis[2].style.color = "red";
-// lis[3].textContent = "hello"
-
-// //convert HTML collection into array
-
-// lis = Array.from(lis)
-
-// lis.forEach(function(li){
-// console.log(li.className)
-// li.textContent = "hello"
-
-// })
-// console.log(lis)
-
-//document.querySelectorAll
-const items = document.querySelectorAll("ul.collection li.collection-item")
-
-items.forEach(function(item, index){
-
-  item.textContent = `${index}: hello`
-
-})
-
-
-
-const liOdd = document.querySelectorAll("li:nth-child(odd)")
-const liEven = document.querySelectorAll("li:nth-child(even)")
-
-liOdd.forEach(function(li, index){
-li.style.background = "#ccc"
-
-})
-
-
-liEven.forEach(function(li, index){
-  li.style.background = "#fefe"
-  
-  })
-  
-
-console.log(items)
+console.log(val)
