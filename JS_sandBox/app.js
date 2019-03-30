796a18558ca637456ee53ef0ad0da6f6a9584c05
edgarated <[chukwuebukaaneke22@gmail.com]>
2019-03-30 07:13:18 +0100

    Creating-Elements

--- JS_sandBox/app.js
+++ JS_sandBox/app.js
@@ -1,39 +1,23 @@
-//Traversing-The-DOM 
+//create  element
+const li = document.createElement("li");
+//add class
+li.className = "collection-item"
+//add content
+const item = document.createTextNode("Hello");
+//append content to li
+li.appendChild(item)
 
-let val;
-const list = document.querySelector("ul.collection");
-const listItem = document.querySelector("li.collection-item:first-child")
+// create content for link
+const link = document.createElement("a");
+//add class
+link.className="delete-item secondary-content"
+//add second link using innerHTML
+link.innerHTML= '<i class="fa fa-remove"></i>'
+//append link to li
+li.appendChild(link)
+//append li to ul
+document.querySelector("ul.collection").appendChild(li)
 
-val = listItem;
-val = list;
+console.log(li)
 
-//Get child nodes
 
-val = list.childNodes
-
-//get children element nodes
-
-val = list.children
-val = list.children[4].textContent = "Hello"
-
-//get children of children
-
-val = list.children[2].children[0].id = "text-link"
-val = list.children[2].children[0]
-
-val = list.firstElementChild
-val = list.lastElementChild
-
-//Get parent node
-
-val = listItem.parentNode
-val = listItem.parentElement
-val = listItem.parentElement.parentElement
-
-//get next sibling
-val = listItem.nextElementSibling.nextElementSibling.nextElementSibling.previousElementSibling
-
-//get prev sibling
-// val = listItem.previousElementSibling
-
-console.log(val)
